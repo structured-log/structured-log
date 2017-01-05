d4e382b657ab6ed4606f222ec947368676afc938
Martin Wedvich <wedvich@gmail.com>
2017-01-05 04:58:42 +0100

    Fixed borked import

--- test/loggerConfiguration.spec.ts
+++ test/loggerConfiguration.spec.ts
@@ -5,7 +5,7 @@
 import { expect } from 'chai';
 import * as TypeMoq from 'typemoq';
 import { LoggerConfiguration } from '../src/loggerConfiguration';
-import { Logger } from '../src/Logger';
+import { Logger } from '../src/logger';
 import { LogEventLevel } from '../src/logEvent';
 import { Pipeline, PipelineStage } from '../src/pipeline';
 import { Sink, SinkStage } from '../src/sink';
